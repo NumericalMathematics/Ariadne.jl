81a6a160ca9c87648c4a082f8e4e8df39fcf254a
MarcoArtiano <artianomarco97@gmail.com>
2025-06-26 14:48:45 +0200

    some cleanup

--- examples/trixi_rosenbrock.jl
+++ examples/trixi_rosenbrock.jl
@@ -19,59 +19,15 @@ using CairoMakie
 @assert !Trixi._PREFERENCE_POLYESTER
 @assert !Trixi._PREFERENCE_LOOPVECTORIZATION
 
-using OrdinaryDiffEq
-using Trixi
-
-###############################################################################
-# semidiscretization of the linear advection equation
-
-advection_velocity = (0.2, -0.7)
-equations = LinearScalarAdvectionEquation2D(advection_velocity)
-
-# Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
-solver = DGSEM(polydeg = 3, surface_flux = flux_lax_friedrichs)
-
-coordinates_min = (-1.0, -1.0) # minimum coordinates (min(x), min(y))
-coordinates_max = (1.0, 1.0) # maximum coordinates (max(x), max(y))
-
-# Create a uniformly refined mesh with periodic boundaries
-mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level = 4,
-                n_cells_max = 30_000) # set maximum capacity of tree data structure
-
-# A semidiscretization collects data structures and functions for the spatial discretization
-semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_convergence_test,
-                                    solver)
-
-###############################################################################
-# ODE solvers, callbacks etc.
-
-# Create ODE problem with time span from 0.0 to 1.0
-ode = semidiscretize(semi, (0.0, 1.0))
-
-# At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
-# and resets the timers
-summary_callback = SummaryCallback()
-
-# The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
-analysis_callback = AnalysisCallback(semi, interval = 100)
-
-# The SaveSolutionCallback allows to save the solution to a file in regular intervals
-save_solution = SaveSolutionCallback(interval = 100,
-                                     solution_variables = cons2prim)
-
-# The StepsizeCallback handles the re-calculation of the maximum Δt after each time step
-
-# Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
-callbacks = CallbackSet(summary_callback, analysis_callback, save_solution)
+trixi_include(joinpath(examples_dir(), "tree_2d_dgsem", "elixir_advection_basic.jl"), cfl = 10.0, sol = nothing);
 
 ###############################################################################
 # run the simulation
 
 sol = solve(
-    ode, Theseus.Rosenbrock();
-    dt = 0.01, # solve needs some value here but it will be overwritten by the stepsize_callback
-    ode_default_options()..., callback = callbacks,
-    # verbose=1,
-    krylov_algo = :gmres,
+	ode, Theseus.Rosenbrock();
+	dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+	ode_default_options()..., callback = callbacks,
+	# verbose=1,
+	krylov_algo = :gmres,
 );
