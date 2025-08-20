17c75e6c972e0254158593e63f97e5252089b83c
Valentin Churavy <v.churavy@gmail.com>
2025-08-20 17:37:05 +0200

    only compar TRBDF2

--- examples/trixi.jl
+++ examples/trixi.jl
@@ -15,17 +15,14 @@ import Ariadne: JacobianOperator
 # ```toml
 # [Trixi]
 # loop_vectorization = false
-# polyester = false
+# backend = "static"
 # ```
 
 @assert Trixi._PREFERENCE_THREADING !== :polyester
 @assert !Trixi._PREFERENCE_LOOPVECTORIZATION
 
 # ## Load Trixi Example
-## trixi_include(joinpath(examples_dir(), "tree_2d_dgsem", "elixir_advection_basic.jl"), sol = nothing);
-trixi_include(joinpath(examples_dir(), "tree_2d_dgsem", "elixir_advection_basic.jl"));
-
-ref = copy(sol)
+trixi_include(joinpath(examples_dir(), "tree_2d_dgsem", "elixir_advection_basic.jl"), sol = nothing);
 
 u = copy(ode.u0)
 du = zero(ode.u0)
@@ -39,44 +36,14 @@ v = zero(u)
 
 ## precompile
 mul!(u, J, v)
+F!(res, u, (ode.u0, 1.0, du, ode.p, 0.0, (), 1))
 
 @time mul!(u, J, v)
 @time F!(res, u, (ode.u0, 1.0, du, ode.p, 0.0, (), 1))
 
 # Cost of time(mul!) ≈ 2 * time(F!)
 
-# ### Jacobian (of the implicit function given the ode)
-# J = Theseus.jacobian(Theseus.ImplicitEuler(), ode, 1.0)
-
 # ### Solve using ODE interface
-
-sol_euler = solve(
-    ode, Theseus.ImplicitEuler();
-    dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-    ode_default_options()..., callback = callbacks,
-    ## verbose=1,
-    krylov_algo = :gmres,
-    ## krylov_kwargs=(;verbose=1)
-);
-
-sol_midpoint = solve(
-    ode, Theseus.ImplicitMidpoint();
-    dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-    ode_default_options()..., callback = callbacks,
-    ## verbose=1,
-    krylov_algo = :gmres,
-    ## krylov_kwargs=(;verbose=1)
-);
-
-sol_trapezoid = solve(
-    ode, Theseus.ImplicitTrapezoid();
-    dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
-    ode_default_options()..., callback = callbacks,
-    ## verbose=1,
-    krylov_algo = :gmres,
-    ## krylov_kwargs=(;verbose=1)
-);
-
 
 sol_trbdf2 = solve(
     ode, Theseus.TRBDF2();
@@ -87,6 +54,14 @@ sol_trbdf2 = solve(
     ## krylov_kwargs=(;verbose=1)
 );
 
+# #### Plot the solution
+
+# We have to manually convert the sol since Theseus has it's own leightweight solution type.
+
+plot(Trixi.PlotData2DTriangulated(sol_trbdf2.u[end], sol_trbdf2.prob.p))
+
+# ### Solve using OrdinaryDiffEqSDIRKSDIRK
+
 import OrdinaryDiffEqSDIRK
 import DifferentiationInterface: AutoFiniteDiff
 sol_sdrik = solve(
@@ -96,17 +71,9 @@ sol_sdrik = solve(
     adaptive = false
 );
 
-# ### Plot the (reference) solution
+# #### Plot the solution
 
-# We have to manually convert the sol since Theseus has it's own leightweight solution type.
-# Create an extension.
-## pd = PlotData2D(sol.u[end], sol.prob.p)
-
-plot(Trixi.PlotData2DTriangulated(ref.u[1], ref.prob.p))
-
-# ### Plot the solution
-
-plot(Trixi.PlotData2DTriangulated(sol_trbdf2.u[end], sol.prob.p))
+plot(Trixi.PlotData2DTriangulated(sol_sdrik.u[end], sol_sdrik.prob.p))
 
 # ## Increase CFL numbers
 
