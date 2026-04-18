2bca4c5eba4d9467f3e67e9fa6f23b67c04e40c9
Valentin Churavy <v.churavy@gmail.com>
2026-04-18 12:39:37 +0200

    Apply suggestions from code review
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/linesearches.jl
+++ src/linesearches.jl
@@ -5,7 +5,7 @@ using LinearAlgebra
 """
     AbstractLineSearch
 
-Linesearch may update the solution `u` and the residual `res` in-place,
+Line search may update the solution `u` and the residual `res` in-place,
 given the function `F!`, parameters `p`, and the Newton direction `d`.
 
 They must call `F!(res, u, p)` to update the residual after updating `u`.
@@ -29,7 +29,13 @@ end
 """
     BacktrackingLineSearch(; n_iter_max = 10, parabolic = false)
 
-After SIAMFANLEquations.jl
+## References
+
+- Kelley, C. T. (2022).
+  Solving nonlinear equations with iterative methods:
+  Solvers and examples in Julia.
+  Society for Industrial and Applied Mathematics.
+- <https://github.com/ctkelley/SIAMFANLEquations.jl>
 """
 Base.@kwdef struct BacktrackingLineSearch <: AbstractLineSearch
     n_iter_max::Int = 10
@@ -37,7 +43,7 @@ Base.@kwdef struct BacktrackingLineSearch <: AbstractLineSearch
 end
 
 function (ls::BacktrackingLineSearch)(F!, res, n_res_prior, u, p, d)
-    alpha = 1.e-4
+    alpha = 1.0e-4
     lambda = 1.0
 
     @assert ls.n_iter_max > 0 "n_iter_max must be positive"
