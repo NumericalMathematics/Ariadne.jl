fe16d3b43e5f98e4ea94c0cef58ad5bc18f0f260
Valentin Churavy <v.churavy@gmail.com>
2026-04-20 13:21:25 +0200

    enable access to J and make backtracking line search fully in place

--- src/linesearches.jl
+++ src/linesearches.jl
@@ -9,6 +9,20 @@ Line search may update the solution `u` and the residual `res` in-place,
 given the function `F!`, parameters `p`, and the Newton direction `d`.
 
 They must call `F!(res, u, p)` to update the residual after updating `u`.
+
+```julia
+struct NewLineSearch <: AbstractLineSearch
+    # parameters for the line search
+end
+
+function (ls::NewLineSearch)(J::AbstractJacobianOperator, F!, res, norm_res_prior, u, p, d)
+    # perform line search to find an appropriate step size
+    # ...
+    # update u and res in-place
+    F!(res, u, p)
+    return norm(res)
+end
+```
 """
 abstract type AbstractLineSearch end
 
@@ -19,7 +33,7 @@ A line search that does not perform any line search: it simply takes the full Ne
 """
 struct NoLineSearch <: AbstractLineSearch end
 
-function (::NoLineSearch)(F!, res, _, u, p, d)
+function (::NoLineSearch)(J, F!, res, norm_res_prior, u, p, d)
     # No line search: take the full Newton step
     u .+= d
     F!(res, u, p)
@@ -41,32 +55,35 @@ Base.@kwdef struct BacktrackingLineSearch <: AbstractLineSearch
     n_iter_max::Int = 10
 end
 
-function (ls::BacktrackingLineSearch)(F!, res, n_res_prior, u, p, d)
+function (ls::BacktrackingLineSearch)(J, F!, res, norm_res_prior, u, p, d)
     alpha = 1.0e-4
     lambda = 1.0
 
     @assert ls.n_iter_max > 0 "n_iter_max must be positive"
     @assert alpha > 0 "alpha must be positive"
 
-    n_res = Inf
-    u_trial = similar(u)
+    # Take a step of size s in the direction d
+    u .= muladd.(lambda, d, u) # u = u + lambda * d
+    F!(res, u, p)
+    norm_res = norm(res)
 
-    for _ in 1:ls.n_iter_max
-        # Take a step of size s
-        u_trial .= muladd.(lambda, d, u) # u = u + lambda * d
-        F!(res, u_trial, p)
-        n_res = norm(res)
-
+    previous_lambda = lambda
+    for _ in 2:ls.n_iter_max
         # Armijo condition
-        if n_res <= (1 - alpha * lambda) * n_res_prior
-            u .= u_trial
-            return n_res
+        if norm_res <= (1 - alpha * lambda) * norm_res_prior
+            return norm_res
         end
 
+        s = previous_lambda - lambda
+        # Take a step of size s in the direction d
+        u .= muladd.(s, d, u) # u = u + s * d
+        F!(res, u, p)
+        norm_res = norm(res)
+
+        previous_lambda = lambda
         lambda *= 0.5
     end
-    u .= u_trial
-    return n_res
+    return norm_res
 end
 
 end # module LineSearches
