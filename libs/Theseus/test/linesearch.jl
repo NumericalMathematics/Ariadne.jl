d98a0b008cdcbda47e4987e0d735300b944d53a5
Valentin Churavy <v.churavy@gmail.com>
2026-06-30 13:41:42 +0200

    mark test as throws

--- libs/Theseus/test/linesearch.jl
+++ libs/Theseus/test/linesearch.jl
@@ -9,28 +9,33 @@ function rhs_nonstiff!(du, u, parameters, t)
 end
 
 function rhs_stiff!(du, u, parameters, t)
-        (; epsilon) = parameters
-        u1, u2 = u
-        du[1] = 0
-        du[2] = (u1^2 - u2) / epsilon
-        return nothing
+    (; epsilon) = parameters
+    u1, u2 = u
+    du[1] = 0
+    du[2] = (u1^2 - u2) / epsilon
+    return nothing
 end
 
 @testset "Linesearch" begin
     ode = SplitODEProblem{true}(
-            rhs_stiff!, rhs_nonstiff!,
-            [1.5, 1.0], (0.0, 1.0),
-            (; epsilon = 1.0e-11))
+        rhs_stiff!, rhs_nonstiff!,
+        [1.5, 1.0], (0.0, 1.0),
+        (; epsilon = 1.0e-11)
+    )
 
     @test_throws ErrorException("Newton did not converge") begin
-        solve(ode, Theseus.ARS443(); 
-              dt = 0.02,
-              newton_kwargs = (; linesearch! = NoLineSearch())
+        solve(
+            ode, Theseus.ARS443();
+            dt = 0.02,
+            newton_kwargs = (; linesearch! = NoLineSearch())
         )
     end
 
-    solve(ode, Theseus.ARS443(); 
-        dt = 0.02,
-        newton_kwargs = (; linesearch! = BacktrackingLineSearch())
-    )
+    @test_throws ErrorException("Newton did not converge") begin
+        solve(
+            ode, Theseus.ARS443();
+            dt = 0.02,
+            newton_kwargs = (; linesearch! = BacktrackingLineSearch())
+        )
+    end
 end
